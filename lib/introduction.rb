69ae50e843c7eeadfd72dde3b8a76f3c4dcd242b
Jaryd Hermann <hermannjaryd@gmail.com>
2021-02-16 23:24:30 +0000

    Automatically backed up by Learn

--- lib/introduction.rb
+++ lib/introduction.rb
@@ -3,5 +3,5 @@ def introduction(name)
 end
 
 def introduction_with_language(name, language)
-  puts "I am #{name} and speak #{language}"
+  puts "Hi, my name is #{name} and I am learning to program #{language}"
 end
